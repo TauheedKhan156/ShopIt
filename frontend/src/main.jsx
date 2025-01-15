4f42f6001ec05d30fadedde13300b97a73d96ad6
John Wick <khantauheed204@gmail.com>
2025-01-15 22:56:02 +0530

    3 days work done

---
+++ frontend/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import App from './App.jsx'
+import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom' 
+import ShopContextProvider  from './context/ShopContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+      <ShopContextProvider>
+        <App/>
+      </ShopContextProvider>
+    </BrowserRouter>
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//24:47 There might be some change
